b344afca80968541a781bd125a30799f37b3da49
azalea-uva <139254414+azalea-uva@users.noreply.github.com>
2025-01-07 09:14:24 -0500

    YLIM

--- code/Electrical/electrical_plots.py
+++ code/Electrical/electrical_plots.py
@@ -824,7 +824,7 @@ class OutputCurve:
         ax.yaxis.major.formatter._useMathText = True
         # NOTE: User can change the x and y limits here
         plt.ylim(
-         0.6E-09,0.7e-09
+         0,2e-09
         )
         # Save the figure
         # NOTE: User can change the filename
